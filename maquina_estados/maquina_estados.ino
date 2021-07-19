c93f1d8b062c663389619b6e1c86c0566a90fc23
Ricardo Nicida Kazama <ricakazama@usp.br>
2021-07-19 14:13:12 -0300

    limpa o codigo da maquina de estados

--- maquina_estados/maquina_estados.ino
+++ maquina_estados/maquina_estados.ino
@@ -14,9 +14,6 @@ int codigoAcao;
 int acao_matrizTransicaoEstados[NUM_ESTADOS][NUM_EVENTOS];
 int proximo_estado_matrizTransicaoEstados[NUM_ESTADOS][NUM_EVENTOS];
 
-//byte TP = 0b10101010; //sinal oposto a cada pino
-
-
 /************************************************************************
  executarAcao
  Executa uma acao
@@ -156,29 +153,9 @@ int obterProximoEstado(int estado, int codigoEvento) {
 void setup()
 {
   Serial.begin(9600);
-  iniciaSistema();
-  
-//  DDRC = 0b11111111; // define todas os pinos analogicos como output
-//  // Inicializa timer 1
-//  noInterrupts();           // desabilita interrupções
-//  TCCR1A = 0;               // configura timer para operação normal
-//  TCCR1B = 0;               // limpa registrador
-//  TCNT1  = 0;               // zera temporizado
-//  OCR1A = 200;              // carrega registrador de comparação:
-//                            //(16MHz / 200 = 80kHz square -> 40kHz full wave)
-//  TCCR1B |= (1 << WGM12);   // CTC mode: possibilita a alternância dos pinos mesmo sem interrupções
-//  TCCR1B |= (1 << CS10);    // inicia o timer Fcpu/64
-//  TIMSK1 |= (1 << OCIE1A);  // habilita interrupção por igualdade de comparação
-//  interrupts();             // habilita interrupções
-  
+  iniciaSistema();  
   Serial.println("Sistema iniciado");
 }
-
-//ISR(TIMER1_COMPA_vect)    // interrupção por igualdade de comparação no TIMER1        
-//{
-//  PORTC = TP; // envia o valor de TP para as saídas
-//  TP = ~TP;   // inverte o TP para a próxima vez que rodar
-//}
 
 void loop(){
   if (eventoInterno == NENHUM_EVENTO) {
